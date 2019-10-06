484f1ca955ab4e25feb305d0be04f0e609f602ab
Mensur Bektic <mensurbektic@Mensurs-MacBook-Pro.local>
2019-10-06 19:48:10 +0200

    Added initial screens and setup.

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = e => {
+    const { value, name } = e.target;
+
+    this.setState({ [name]: value })
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label="Email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="Password"
+            required
+          />
+          <CustomButton type="submit" value="Submit"> Sign in </CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
