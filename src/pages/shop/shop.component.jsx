e2344fe9e6f5e2016e4da1491a86d8ad364e5758
Mensur Bektic <mensurbektic@Mensurs-MacBook-Pro.local>
2019-11-14 20:43:21 +0100

    Added spinner HOC.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,47 @@
 import React from "react";
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import CollectionPage from '../collection/collection.component';
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
-  </div>
-);
+import WithSpinner from "../../components/with-spinner/withSpinner.component";
+
+const WithSpinnerCollectionsOverview = WithSpinner(CollectionsOverview);
+
+class ShopPage extends React.PureComponent {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false })
+    }, 1500);
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+
+    return (
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          render={() => (
+            <WithSpinnerCollectionsOverview
+              isLoading={loading}
+              {...this.props}
+            />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
